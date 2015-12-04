b52f59b6747d02f97a960bcd6366f8b1e9668823
Dan Comand <dcomand@gmail.com>
2015-12-03 21:22:33 -0500

    added code to check off returned items, and show the amount of items that have been lent out.

--- simple-strings.js
+++ simple-strings.js
@@ -4,8 +4,18 @@ if (Meteor.isClient) {
   // counter starts at 0
   Template.body.helpers({
     strings: function () {
+      if (Session.get("hideCompleted")) {
+        return Strings.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
       return Strings.find({}, {sort: {createdAt: -1}});
     }
+   },
+   hideCompleted: function () {
+    return Session.get("hideCompleted");
+   },
+   incompleteCount: function () {
+    return Strings.find({checked: {$ne: true}}).count();
+   } 
   });
 
   Template.body.events({
@@ -23,7 +33,9 @@ if (Meteor.isClient) {
 
       //clear form
       event.target.text.value = "";
-
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
